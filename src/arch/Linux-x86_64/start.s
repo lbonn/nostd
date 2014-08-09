5c5e44071a9d7693f7c855a06d51732e626ea0e8
lbonn <bonnans.l@gmail.com>
2014-08-10 00:18:09 +0200

    _start: correctly align rsp to comply with abi

--- src/arch/Linux-x86_64/start.s
+++ src/arch/Linux-x86_64/start.s
@@ -5,6 +5,7 @@
 _start:
     popq %rdi       /* argc */
     movq %rsp, %rsi /* argv */
+    andq $0xffffffffffffff00, %rsp /* abi alignment */
     call main
     movq %rax, %rdi
     movq $60, %rax
